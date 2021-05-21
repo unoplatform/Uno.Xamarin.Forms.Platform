37c48ebc871904a25401f967b650d50181bb551e
Jerome Laban <jerome.laban@nventive.com>
2021-05-20 22:37:09 -0400

    ci: Update test init script

--- build/scripts/wasm-uitest-run.sh
+++ build/scripts/wasm-uitest-run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd $BUILD_SOURCESDIRECTORY
 
-msbuild /r /p:Configuration=Release /p:DisableOriginalVersioning=true $BUILD_SOURCESDIRECTORY/Xamarin.Forms.ControlGallery.Uno.Wasm/Xamarin.Forms.ControlGallery.Uno.Wasm.csproj
+dotnet build /r /p:Configuration=Release /p:DisableOriginalVersioning=true $BUILD_SOURCESDIRECTORY/Xamarin.Forms.ControlGallery.Uno.Wasm/Xamarin.Forms.ControlGallery.Uno.Wasm.csproj
 msbuild /r /p:Configuration=Release /p:DisableOriginalVersioning=true $BUILD_SOURCESDIRECTORY/Xamarin.Forms.Core.UITests.Wasm/Xamarin.Forms.Core.UITests.Wasm.csproj
 
 cd $BUILD_SOURCESDIRECTORY/build
@@ -12,6 +12,10 @@ cd $BUILD_SOURCESDIRECTORY/build
 npm i chromedriver@74.0.0
 npm i puppeteer@1.13.0
 mono $BUILD_SOURCESDIRECTORY/build/NuGet.exe install NUnit.ConsoleRunner -Version 3.10.0
+
+# install dotnet serve
+dotnet tool install dotnet-serve --version 1.8.15 --tool-path $BUILD_SOURCESDIRECTORY/build/tools
+export PATH="$PATH:$BUILD_SOURCESDIRECTORY/build/tools"
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
@@ -22,8 +26,7 @@ export UNO_UITEST_CHROME_CONTAINER_MODE=true
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
-## The python server serves the current working directory, and may be changed by the nunit runner
-bash -c "cd $BUILD_SOURCESDIRECTORY/Xamarin.Forms.ControlGallery.Uno.Wasm/bin/Release/netstandard2.0/dist/; python server.py &"
+dotnet serve -p 8000 -d "$BUILD_SOURCESDIRECTORY/Xamarin.Forms.ControlGallery.Uno.Wasm/bin/Release/net5.0/dist/" &
 
 mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe \
     --trace=Verbose --inprocess --agents=1 --workers=1 \
